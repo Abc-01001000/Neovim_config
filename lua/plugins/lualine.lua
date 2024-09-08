e9e5d6c53366e4c8e8213fedb0ead205c10a7f4e
何家伟 <138421633+Abc-01001000@users.noreply.github.com>
2024-09-08 18:46:32 +0800

    Update lualine.lua
    rm Transparent config in luaLine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,19 +2,19 @@ require('lualine').setup({
     options = {
         theme = 'tokyonight-night' 
     },
-    sections = {
-        lualine_a = {
-            {
-                'mode',
-                color = { fg = '#abcdef' }, -- Red font, black background, bold text_align
-            },
-        },
+    -- sections = {
+    --     lualine_a = {
+    --         {
+    --             'mode',
+    --             color = { fg = '#abcdef' }, -- Red font, black background, bold text_align
+    --         },
+    --     },
 
-        lualine_z = {
-            {
-                'location',
-                color = { fg = '#abcdef'},
-            },
-        },
-    },
+    --     lualine_z = {
+    --         {
+    --             'location',
+    --             color = { fg = '#abcdef'},
+    --         },
+    --     },
+    -- },
 })
